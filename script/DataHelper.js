e6b918408aa2c470d7dd9d828b4829da5d1c4df6
sergiam <sergi@bcngeek.com>
2022-10-26 13:11:12 +0200

    finished minesweeper (?), working on steps and tests

--- script/DataHelper.js
+++ script/DataHelper.js
@@ -1,56 +1,7 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const mockData = urlParams.get('mockData');
-console.log(mockData);
 var board = [];
-
-
-/*
-const parseMockDataToString = (data) => {
-    let strData = data.split(/\r?\n/).join('-')
-    strData = strData.replaceAll(' ', '')
-    strData = strData.replaceAll('|', '')
-    while (strData[strData.length - 1] === '-') {
-      strData = strData.slice(0, -1)
-    }
-    return strData
-}
-
-
-//Toni's functions for validating and parsing the mockdata
-const validateMockData = (data) => {
-    let isValidData
-    if (data === undefined) {
-      isValidData = false
-    } else if (data.includes('-')) {
-      isValidData = validateMockDataRows(data.split('-'))
-    } else {
-      isValidData = validateMockDataRow(data)
-    }
-    return isValidData
-}
-
-//Toni's functions for validating and parsing the mockdata
-const validateMockDataRow = (data) => {
-    const newLocal = '^[*o]*$'
-    const regex = new RegExp(newLocal)
-    return regex.test(data)
-}
-
-//Toni's functions for validating and parsing the mockdata
-const validateMockDataRows = (dataRows) => {
-    const currentLenght = dataRows[0].length
-    let isValidData
-    for (let i = 0; i < dataRows.length; i += 1) {
-      if (dataRows[i].length !== currentLenght) {
-        isValidData = false
-        break
-      }
-      isValidData = validateMockDataRow(dataRows[i])
-    }
-    return isValidData
-}
-*/
 
 //Toni's functions for validating and creating the board from mock data
 const createBoardFromMockData = (data) => {
@@ -68,11 +19,10 @@ const createBoardFromMockData = (data) => {
           userTag: '',
           isWrongTagged: false,
           numberOfMinesAround: 0,
-          isMine: mockBoard[row][column] === '*'
+          isMine: (mockBoard[row][column] === '*')
         })
       }
     }
-    console.log('board', board)
     return board
 }
 
@@ -93,8 +43,46 @@ const createBoard = () => {
       })
     }
   }
-  console.log('board', board)
   return board
+}
+
+function isWin() {
+  let win = true;
+
+  for(let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j].isMine == false) {
+        if (board[i][j].isRevealed == false) {
+          win = false;
+        }
+      }
+    }
+  }
+  return win;
+}
+
+function setUserTag(cellId) {
+  
+  let id = cellId.split("-");
+  let row = id[0];
+  let column = id[1];
+  let userTag = board[row][column].userTag;
+if (board[row][column].isRevealed == false) {
+  if(board[row][column].userTag == '') {
+    userTag = '!';
+  }
+  if(board[row][column].userTag == '!') {
+    userTag = '?';
+  }
+  if(board[row][column].userTag == '?') {
+    userTag = '';
+  }
+  board[row][column].userTag = userTag;
+  return board[row][column].userTag; 
+  
+}
+ 
+  
 }
 
 function setRandomMines() {
--- script/minesweeper.js
+++ script/minesweeper.js
@@ -1,129 +1,239 @@
 const columns = 8;
 const rows = 8;
-const mineCounter = 10;
+let mineCounter = 10;
+let time;
+let startClick = false;
+let finishGame = false;
 
-
-//const mineField = [];
 addEventClick();
 
-/*
-//Características de cada casilla (Tablero)
-function minefieldCreation(){
-  for (let i = 0; i < columns; i++){
-      mineField.push([])
-          for (let j = 0; j < rows; j++){
-              mineField[i].push({
-                  mine:false, //hay mina en dicha casilla, mineCounter? 
-                  hidden:true, //oculta o revelada
-                  num:null
-              })
-          }
-  }
-  
-}
-*/
+document.addEventListener("DOMContentLoaded", () =>  {
+    startGame();
+});
 
-document.addEventListener('DOMContentLoaded', () => 
-{
-    if(window.location.search.includes('?')){
-        console.log("mockData detected");
+function startGame() {
+    if (window.location.search.includes("?")) {
         board = createBoardFromMockData(mockData);
-        console.log(board);
-        generateTable(board.length,board.length);
-        addEventClick();  
-    }else{
-//        minefieldCreation();
-        console.log("no mockData")
-        board = createBoard(columns,rows);
-        generateTable(columns,rows);
+        generateTable(board.length, board[0].length);
+        addEventClick();
+      } else {
+        board = createBoard(columns, rows);
+        generateTable(columns, rows);
         setRandomMines();
         addEventClick();
     }
-})
+}
 
 function addEventClick() {
-    //var cell = document.createElement("td");
-    let cells = document.getElementsByTagName("td");
-    for (const elements of cells) {
-        elements.addEventListener('click', () => {
-            uncoverCell(elements.getAttribute("id"));
-            
-        });
+  //var cell = document.createElement("td");
+  let cells = document.getElementsByTagName("td");
+
+  for (const elements of cells) {
+    elements.addEventListener("click", () => {
+      uncoverCell(elements.getAttribute("id"));
+      win();
+    });
+  }
+  for (const elements of cells) {
+    elements.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+      taggedCell(elements.getAttribute("id"));
+    });
+  }
+
+  document.getElementById("face").addEventListener("click", () => {
+    resetGame();
+});
+}
+
+function resetGame() {
+    startGame();
+    clearInterval(time);
+    startClick = false;
+    finishGame = false;
+    document.getElementById("time-counter").innerText = "Time counter: 0";
+    mineCounter = 10;
+    document.getElementById("mine-counter").innerText = "Mine counter: 10";
+    document.getElementById("face").setAttribute("src", "/img/normal_face.png");
+
+}
+
+function setTime() {
+  startClick = true;
+  let second = 1;
+  time = window.setInterval(function () {
+    document.getElementById("time-counter").innerHTML =
+      "Time Counter: " + second;
+    second++;
+  }, 1000);
+}
+
+function taggedCell(cellId) {
+  let cell = document.getElementById(cellId);
+  if (!finishGame) {
+    if (!startClick) {
+      setTime();
     }
+
+    let cellTag = setUserTag(cellId);
+    console.log(cellTag);
+
+    if (cellTag == "!") {
+      cell.classList.add("minetag");
+      mineCounter--;
+    }
+    if (cellTag == "?") {
+      cell.classList.remove("minetag");
+      cell.innerText = "?";
+      mineCounter++;
+    }
+    if (cellTag == "") {
+        console.log("entra");
+      cell.innerText = "";
+
+    }
+  } else {
+    cell.innerText = "";
+    cell.classList.add("minetag");
+  }
+  document.getElementById("mine-counter").innerHTML =
+    "Mine Counter: " + mineCounter;
 }
 
 function uncoverCell(cellId) {
-    let cell = document.getElementById(cellId);
-    let row = cellId[0];
-    let column = cellId[2];
-    board[row][column].isRevealed = true;
-    showCell(cellId);
+  if (!startClick) {
+    setTime();
+  }
+  let cell = document.getElementById(cellId);
+  let row = cellId[0];
+  let column = cellId[2];
+  board[row][column].isRevealed = true;
+  showCell(cellId);
 
-    cell.classList.add("celluncovered");
-    cell.classList.remove("cellcovered");
+  cell.classList.add("celluncovered");
+  cell.classList.remove("cellcovered");
 }
 
 function coverCell(cellId) {
-    let cell = document.getElementById(cellId);
-    cell.classList.add("cellcovered");
-    cell.classList.remove("celluncovered");
-    
+  let cell = document.getElementById(cellId);
+  cell.classList.add("cellcovered");
+  cell.classList.remove("celluncovered");
 }
 
-function showCell(cellId) { 
-    let cells = document.getElementById(cellId);
-    let row = cellId[0];
-    let column = cellId[2];
-    if (board[row][column].isMine == true) {
-        cells.classList.add("mine")
+function showCell(cellId) {
+  console.log(board);
+  let cells = document.getElementById(cellId);
+  let row = cellId[0];
+  let column = cellId[2];
+
+  if (board[row][column].isMine == true) {
+    cells.classList.add("mine");
+    defeat(cellId);
+  } else {
+    let minesaround = minesAround(row, column);
+    if (minesaround != 0) {
+      board[row][column].isRevealed = true;
+      cells.innerText = minesaround;
     } else {
-        let minesaround = minesAround(row,column);
-        if (minesaround != 0) {
-            cells.innerText = minesaround;
-        } else {
-            
-        }
+      cells.classList.add("celluncovered");
+      board[row][column].isRevealed = true;
+      showSurroundedCells(row, column);
     }
-
-    defeat();
-}
-
-
-function defeat(cellId) {
-   for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board.height; j++) {
-            if (board[i][j].isMine) {
-
-                let cell = document.getElementById(cellId)
-
-                if (board[i][j].isRevealed == true && board[i][j].isMine === '*') {
-                cell.setAttribute("disabled", true);
-                }
-                
-                cell.classList.add("celluncovered");
-                cell.classList.remove("cellcovered");
-                cell.classList.add("mine");
-            }
-        }
-    }
-}
-
-
-//Tabla sin mockdata, 8x8
-function generateTable(height,width){
-  var table = document.getElementById("board-inside-content");
-  for (let i = 0; i < height; i++){
-      var row = document.createElement("tr");
-      row.setAttribute("id","row:"+i);
-      for (let j = 0; j < width; j++){
-          var cell = document.createElement("td");
-          var id =i+"-"+j;
-          var classCell= "cellcovered";
-          cell.classList.add(classCell);
-          cell.setAttribute("id",id);
-          row.appendChild(cell);
-      }
-      table.appendChild(row);
   }
 }
 
+function showSurroundedCells(row, column) {
+  row = parseInt(row);
+  column = parseInt(column);
+
+  if (column < board[row].length - 1) {
+    if (board[row][column + 1].isRevealed == false)
+      document.getElementById(row + "-" + (column + 1)).click();
+  }
+
+  if (column > 0) {
+    if (board[row][column - 1].isRevealed == false)
+      document.getElementById(row + "-" + (column - 1)).click();
+  }
+
+  if (row > 0) {
+    if (board[row - 1][column].isRevealed == false)
+      document.getElementById(row - 1 + "-" + column).click();
+
+    if (column < board[row].length - 1) {
+      if (board[row - 1][column + 1].isRevealed == false)
+        document.getElementById(row - 1 + "-" + (column + 1)).click();
+    }
+    if (column > 0) {
+      if (board[row - 1][column - 1].isRevealed == false)
+        document.getElementById(row - 1 + "-" + (column - 1)).click();
+    }
+  }
+  if (row < board.length - 1) {
+    if (board[row + 1][column].isRevealed == false)
+      document.getElementById(row + 1 + "-" + column).click();
+
+    if (column > 0) {
+      if (board[row + 1][column - 1].isRevealed == false)
+        document.getElementById(row + 1 + "-" + (column - 1)).click();
+    }
+
+    if (column < board[row].length - 1) {
+      if (board[row + 1][column + 1].isRevealed == false)
+        document.getElementById(row + 1 + "-" + (column + 1)).click();
+    }
+  }
+}
+
+function defeat() {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j].isMine == true) {
+        if (board[i][j].isRevealed == false) {
+          let cells = document.getElementById(i + "-" + j);
+          cells.classList.add("mine");
+          cells.classList.add("celluncovered");
+          cells.classList.remove("cellcovered");
+        }
+      }
+    }
+  }
+  clearInterval(time);
+  document.getElementById("face").setAttribute("src", "/img/sad_face.png");
+}
+
+function win() {
+  let win = isWin();
+
+  if (win) {
+    finishGame = true;
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[0].length; j++) {
+        if (board[i][j].isMine == true) {
+          taggedCell(i + "-" + j);
+        }
+      }
+    }
+    document.getElementById("face").setAttribute("src", "/img/happy_face.png");
+    clearInterval(time);
+  }
+}
+
+//Tabla sin mockdata, 8x8
+function generateTable(height, width) {
+  var table = document.getElementById("board-inside-content");
+  table.innerHTML = "";
+  for (let i = 0; i < height; i++) {
+    var row = document.createElement("tr");
+    row.setAttribute("id", "row:" + i);
+    for (let j = 0; j < width; j++) {
+      var cell = document.createElement("td");
+      var id = i + "-" + j;
+      var classCell = "cellcovered";
+      cell.classList.add(classCell);
+      cell.setAttribute("id", id);
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
+}
